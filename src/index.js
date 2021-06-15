1ee1759656183d6ab8257acf6beeaab530a900d9
Felipe Fogaça <felipe.foogaca@gmail.com>
2021-06-14 21:20:59 -0300

    First commit

--- src/index.js
+++ src/index.js
@@ -10,19 +10,69 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if(!user)
+    return response.status(404).json({error:"user not found."});
+
+  request.user = user;
+
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  
+  const userIsPro = user.pro;
+  const userHasTeenTodos = user.todos.length >= 10;
+  
+  if(!userIsPro){
+    if(userHasTeenTodos){
+      return response.status(403).json({error :"User has more than 10 todos."});
+    }
+  }
+
+  next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+
+  const isUuid = validate(id);
+  if(!isUuid)
+    return response.status(400).json({error:"id must be a uuid."});
+
+  const user = users.find((user) => user.username === username);
+
+  if(!user)
+    return response.status(404).json({error:"User not foud."});
+
+  const todo = user.todos.find((todo) => todo.id === id);
+  
+  if(!todo)
+    return response.status(404).json({error:"Todo not found."});
+
+  request.user = user;
+  request.todo = todo;
+  
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  console.log(id);
+
+  const user = users.find((user)=> user.id === id);
+  
+  if(!user)
+    return response.status(404).json({error:"User not found."})
+
+  request.user = user;
+  next();
 }
 
 app.post('/users', (request, response) => {
